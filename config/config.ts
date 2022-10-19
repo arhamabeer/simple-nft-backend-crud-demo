83361458a14d5a3fc6ad86a9b19e24cc2b48f4be
ShayanHussainSB <arhamabeerahmed@gmail.com>
2022-10-19 17:11:05 +0500

    envs

--- config/config.ts
+++ config/config.ts
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
 import router from "../router/main_router";
 import { Express } from "express";
-const db_uri =
-  "mongodb+srv://arham:arham@nodejs.x48o2hy.mongodb.net/?retryWrites=true&w=majority";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+const db_uri = process.env.MDB_URI || "";
 export default function Config(server: Express) {
   mongoose
     .connect(db_uri, { dbName: "DemoApp" })
